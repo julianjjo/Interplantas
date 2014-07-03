8ad4e9bbf65f1b5f84a2ce18a29ff93b180e5494
global <global>
2014-07-03 08:18:25 -0500

    Features y steps en español y escenario mirar plantas de una variedad funcionando

--- features/mostrar_planta.feature
+++ features/mostrar_planta.feature
@@ -1,17 +1,26 @@
-Feature: Mostrar las plantas
+# language: es
+Característica: Mostrar las plantas
   Yo como viverista 
   Deseo poder mirar las plantas del vivero que quiero comprar
   Para comprarlas
 
-  Background:
-    Given Que el viverista desea mirar las plantas del vivero   
+  Antecedentes:
+    Dado que el viverista desea mirar las plantas del vivero   
 
-  Scenario: Mostrar pagina del vivero  	   
-  	When Da click en el link Vivero de la barra de navegación  
-    Then Abrira la pagina que muestra las variedades de las plantas con el titulo "Vivero"
+  Escenario: Mostrar pagina del vivero  	   
+  	Cuando da click en el link Vivero de la barra de navegación  
+    Entonces Abrira la pagina que muestra las variedades de las plantas con el titulo "Vivero"
   
-  Scenario: Mostrar las variedades de las plantas
-  	And busco la variedad con el nombre Cayeno
-  	When Da click en el link Vivero de la barra de navegación 
-  	Then Mostrara la variedad con el nombre "Cayeno"
+  Escenario: Mostrar las variedades de las plantas
+  	Y busco la variedad con el nombre Cayeno
+  	Cuando da click en el link Vivero de la barra de navegación 
+  	Entonces mostrara la variedad con el nombre "Cayeno"
+
+  Escenario: Mirar las plantas de una variedad
+    Dado que busca las plantas de la variedad Cayeno en la pagina vivero
+    Cuando doy click en la variedad Cayeno
+    Entonces mostrara la planta con el nombre "Cayeno Rosado"
+  
+  
+  
   
--- features/step_definitions/interplantas_steps.rb
+++ features/step_definitions/interplantas_steps.rb
@@ -1,23 +1,37 @@
 require "rubygems"
 require 'capybara/rails'
 
-Given(/^Que el viverista desea mirar las plantas del vivero$/) do
+Dado(/^que el viverista desea mirar las plantas del vivero$/) do
   visit '/' 
 end
 
-When(/^Da click en el link Vivero de la barra de navegación$/) do  
+Cuando(/^da click en el link Vivero de la barra de navegación$/) do  
 	click_link 'Vivero'  
 end
 
-Then(/^Abrira la pagina que muestra las variedades de las plantas con el titulo "(.*?)"$/) do |titulo| 
+Entonces(/^Abrira la pagina que muestra las variedades de las plantas con el titulo "(.*?)"$/) do |titulo| 
   expect(page.title).to eq(titulo)
 end
 
-When(/^busco la variedad con el nombre (.*?)$/) do |variedad|
+Cuando(/^busco la variedad con el nombre (.*?)$/) do |variedad|
   @variedad = Variedad.create(nombre: variedad)
 end
 
-Then(/^Mostrara la variedad con el nombre "(.*?)"$/) do |variedad_que_se_espera|
+Entonces(/^mostrara la variedad con el nombre "(.*?)"$/) do |variedad_que_se_espera|
   expect(page).to have_text(variedad_que_se_espera)
 end
 
+Dado(/^que busca las plantas de la variedad (.*?) en la pagina vivero$/) do |variedad|
+  @variedad = Variedad.create(nombre: variedad)
+  visit '/variedades'
+end
+
+Cuando(/^doy click en la variedad Cayeno$/) do
+  @variedad.plantas.build(nombre: "Cayeno Rosado")
+  @variedad.save
+  click_link 'Cayeno'
+end
+
+Entonces(/^mostrara la planta con el nombre "(.*?)"$/) do |planta_que_se_espera|
+  expect(page).to have_text(planta_que_se_espera)
+end
